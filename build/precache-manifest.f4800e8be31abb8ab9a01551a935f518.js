7e5f164d7559051c14666dcba777aacb2160c48d
Andrey Nikolaev <frey.dev@yandex.ru>
2020-10-27 12:21:14 +0300

    add build

---
+++ build/precache-manifest.f4800e8be31abb8ab9a01551a935f518.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1fecad19ff03e9552e42c651f21fe98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbd86b6b49ea731bd257",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "2bd5522670edd34e138c",
+    "url": "/static/css/main.1da4170d.chunk.css"
+  },
+  {
+    "revision": "cbd86b6b49ea731bd257",
+    "url": "/static/js/2.1e15ca01.chunk.js"
+  },
+  {
+    "revision": "2bd5522670edd34e138c",
+    "url": "/static/js/main.914a3e75.chunk.js"
+  },
+  {
+    "revision": "85c541ee2746b53b3cb3",
+    "url": "/static/js/runtime-main.da84b74d.js"
+  },
+  {
+    "revision": "7dc3cc9a9a3c5c1f8aac9cdc7851ddbd",
+    "url": "/static/media/grid.7dc3cc9a.svg"
+  }
+]);
